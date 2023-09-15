415788ad87732ad3f9d5b4ea15c0354719c73ca1
Matheus Felizardo <matheus.felizardo2@gmail.com>
2023-09-15 14:44:13 +0100

    updating colors

--- pages/index.vue
+++ pages/index.vue
@@ -6,31 +6,31 @@
    <div v-if="!$route.params.slug" class="py-5 max-w-5xl m-auto">
       <div class="mb-4  max-w-[75%] m-auto relative flex flex-col justify-center items-center text-black ">
         <div class="flex flex-col items-center mb-8">
-          <h1 class="text-5xl font-thin mb-4">
+          <h1 class="text-5xl font-semibold mb-4">
           {{ posts[0].title }}
           </h1>
 
-          <div class="text-sm mb-6 font-mono font-extralight text-slate-100">
+          <div class="text-md mb-6 font-sans text-black">
           by {{ posts[0].author }}
           </div>
         </div>
 
-         <div class="text-md font-mono" v-html="posts[0].html" />
+         <div class="text-md font-sans" v-html="posts[0].html" />
       </div>
  
       <hr class="mt-2" />
    
-      <div class="my-6 text-sm font-mono text-center uppercase text-black">
+      <div class="my-6 text-sm font-sans text-center uppercase text-black">
          Check all posts
       </div>
       
-     <div id="cards-container" class="grid grid-cols-3 gap-5">
+     <div id="cards-container" class="grid grid-cols-2 gap-5 sm:grid-cols-3">
        <template v-for="(post, index) in posts" :key="post.title">
          <div 
           v-if="index !== 0" 
           id="card" 
-          class="max-w-sm font-mono p-4 relative border text-black border-gray-100 rounded-sm hover:shadow-sm hover:shadow-black ">
-          <NuxtLink class="absolute w-full h-full left-0 top-0 font-mono" :to="`/posts/${post.uri}`"></NuxtLink>
+          class="max-w-sm font-sans p-4 relative border text-black border-gray-100 rounded-sm hover:shadow-sm hover:shadow-black ">
+          <NuxtLink class="absolute w-full h-full left-0 top-0 font-sans" :to="`/posts/${post.uri}`"></NuxtLink>
           <h1 class="text-sm font-thin">
               {{ post.title }}
           </h1>
--- pages/posts/[[slug]].vue
+++ pages/posts/[[slug]].vue
@@ -1,18 +1,18 @@
 <template>
-  <NuxtLink class="p-2 bg-gray-500 text-black mb-4" :to="`/posts`">BACK</NuxtLink>
+  <NuxtLink class="p-2 bg-black text-white mb-4" :to="`/posts`">BACK</NuxtLink>
 
   <div :id="post.title" class="post w-[75%] m-auto p-5 text-black">
       <div class="flex flex-col items-center mb-8">
-        <h1 class="text-5xl font-thin mb-4">
+        <h1 class="text-5xl font-semibold mb-4">
         {{ post.title }}
         </h1>
 
-        <div class="text-sm mb-6 font-mono font-extralight text-slate-100">
+        <div class="text-sm mb-6 font-sans font-extralight text-slate-100">
         by {{ post.author }}
         </div>
       </div>
 
-      <div class="text-md font-mono" v-html="post.html" />
+      <div class="text-md font-sans" v-html="post.html" />
   </div>
 
 </template>
