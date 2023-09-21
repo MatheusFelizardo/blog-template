c95df1dc156ab8485562588f7af522e3ddd35185
Matheus Felizardo <matheus.felizardo2@gmail.com>
2023-09-21 11:25:16 +0100

    updating header menu

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="text-xs text-black font-semibold uppercase flex gap-[72px] justify-center items-center py-8 underline-offset-4 decoration-blue-900 decoration-2">
     <NuxtLink :active-class="'underline '" to="/">Home</NuxtLink>
-    <NuxtLink :active-class="'underline'" to="/about">About me</NuxtLink>
+    <NuxtLink :active-class="'underline'" to="/about">Who am I</NuxtLink>
     <NuxtLink :active-class="'underline'" to="/posts">Stories</NuxtLink>
   </nav>
 </template>
